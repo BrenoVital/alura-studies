92b01ed42706782a43249cde6a2ccaa64997a934
Breno Vital <96732982+BrenoVital@users.noreply.github.com>
2023-06-27 08:38:02 -0300

    Criando o cronometro

---
+++ src/components/Cronometro/Relogio/indext.tsx
@@ -0,0 +1,16 @@
+export const Relogio = () => {
+  return (
+    <>
+    <span>0</span>
+    <span>0</span>
+    <span>:</span>
+    <span>0</span>
+    <span>0</span>
+    {/* <span>:</span>
+    <span>0</span>
+    <span>0</span> */}
+    </>
+  )
+}
+
+export default Relogio;
---
+++ src/components/Cronometro/indext.tsx
@@ -0,0 +1,17 @@
+import Button from "../Button"
+import Relogio from "./Relogio/indext";
+import style from "../Cronometro/Cronometro.module.scss"
+
+export const Cronometro = () => {
+  return(
+    <div className={style.cronometro}>
+      <p className={style.titulo}> Escolha um card e inicie o cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+        <Button text="Começar!" /> 
+    </div>
+  )
+}
+
+export default Cronometro;
