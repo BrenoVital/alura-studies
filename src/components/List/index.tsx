f9b4477ecf4eae4d2d5c1d18381e44dfdc6f26b1
Breno Vital <96732982+BrenoVital@users.noreply.github.com>
2023-07-02 19:54:37 -0300

    Deixando component dinamico com useState

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import { Item } from './Item';
 import style from './style.module.scss';
 const List = () => {
-  const tarefas = [{
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'React',
     tempo: '02:00:00',
     id: 1
@@ -15,10 +16,12 @@ const List = () => {
     tarefa: 'TypeScript',
     tempo: '01:00:00',
     id: 3
-  }]
+  }]);
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={ () => {
+        setTarefas([...tarefas, { tarefa: 'Estados React', tempo: '00:30:00', id: 4 }])
+      }}>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
